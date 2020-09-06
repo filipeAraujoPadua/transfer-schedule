2c014bab96e98885d0d94c773ebbb76b336d1a8e
filipePadua <filipearaujopadua@gmail.com>
2020-09-06 15:32:50 -0300

    configuracao banco em memoria H2

--- transfer-schedule/src/test/java/com/transferSchedule/HelloWorld.java
+++ transfer-schedule/src/test/java/com/transferSchedule/HelloWorld.java
@@ -1,18 +1,18 @@
 package com.transferSchedule;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class TransferScheduleApplicationTests {
+public class HelloWorld {
 
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
-
 }
