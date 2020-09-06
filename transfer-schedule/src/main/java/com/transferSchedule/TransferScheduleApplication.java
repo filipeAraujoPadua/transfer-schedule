7559792e6228b623788be8aaf18380380b55e4a1
filipePadua <filipearaujopadua@gmail.com>
2020-09-06 15:15:27 -0300

    commit inicial

---
+++ transfer-schedule/src/main/java/com/transferSchedule/TransferScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.transferSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferScheduleApplication.class, args);
+	}
+
+}
---
+++ transfer-schedule/src/test/java/com/transferSchedule/TransferScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transferSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
