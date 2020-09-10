1df5519100583bf48de06630627ebb85eccdd21c
filipePadua <filipearaujopadua@gmail.com>
2020-09-10 01:08:47 -0300

    create TransferController

--- transfer-schedule/src/main/java/com/transferSchedule/controller/TransferController.java
+++ transfer-schedule/src/main/java/com/transferSchedule/controller/TransferController.java
@@ -3,6 +3,7 @@ package com.transferSchedule.controller;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.validation.Valid;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -10,8 +11,10 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,8 +35,28 @@ public class TransferController {
 	private TransferRateService transferRateService;
 	
 	@PostMapping
-	public ResponseEntity<Response<TransferDTO>> create (){
-		return null;
+	public ResponseEntity<Response<TransferDTO>> create (@Valid @RequestBody TransferDTO transferDto, BindingResult result) {
+		
+		Response<TransferDTO> response = new Response<TransferDTO>();
+		
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(e -> response.getErrors().add(e.getDefaultMessage()));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		
+		Double rateTransfer = transferRateService.findRate(transferDto.getTransferDate(), transferDto.getSchedulingDate(), transferDto.getTransferAmount());
+		
+		if(rateTransfer == null || rateTransfer == 0) {
+			response.getErrors().add("Não existe nenhuma taxa aplicavel para a transferencia");
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		
+		Transfer transfer = transferService.save(convertDtoToEntity(transferDto));
+		
+		response.setData(convertEntityToDto(transfer));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 	
 	@GetMapping
