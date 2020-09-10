cd66a9321818d2d264b3ccce6df7be5e5408b82c
filipePadua <filipearaujopadua@gmail.com>
2020-09-10 12:55:48 -0300

    TrasferRepositoryTest

--- transfer-schedule/src/main/java/com/transferSchedule/service/TransferRateServiceImpl.java
+++ transfer-schedule/src/main/java/com/transferSchedule/service/TransferRateServiceImpl.java
@@ -9,7 +9,6 @@ import org.joda.time.Period;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.transferSchedule.entity.Transfer;
 import com.transferSchedule.entity.TransferRate;
 import com.transferSchedule.repository.TransferRateRepository;
 
--- transfer-schedule/src/test/java/com/transferSchedule/repository/TransferRepositoryTest.java
+++ transfer-schedule/src/test/java/com/transferSchedule/repository/TransferRepositoryTest.java
@@ -3,6 +3,7 @@ package com.transferSchedule.repository;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,25 +11,40 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.transferSchedule.entity.Transfer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class TransferRepositoryTest {
 
 	@Autowired
-	TransferRepository trasnferRepository;
+	TransferRepository transferRepository;
 	
 	@Before
 	public void setUp() {
 		
+		Transfer transfer = new Transfer();
+		Date transferDate = new Date();
+		Date schedulingDate = new Date();		
+		
+		transfer.setSourceAccount("000000");
+		transfer.setDestinationAccount("000000");
+		transfer.setTransferAmount(100.00);
+		transfer.setTransferRate(30.00);
+		transfer.setTransferDate(transferDate);
+		transfer.setSchedulingDate(schedulingDate);
+
+		transferRepository.save(transfer);
 	}
 	
 	@After
 	public void tearDown() {
 		
+		transferRepository.deleteAll();
 	}
 	
 	@Test
@@ -37,18 +53,23 @@ public class TransferRepositoryTest {
 		Transfer transfer = new Transfer();
 		Date transferDate = new Date();
 		Date schedulingDate = new Date();
-		
-		
+				
 		transfer.setSourceAccount("xxxxxxxx");
 		transfer.setDestinationAccount("XXXXXXXXX");
 		transfer.setTransferAmount(100.00);
-		
-		//Refactor to make calculations based on dates
 		transfer.setTransferRate(30.00);
 		transfer.setTransferDate(transferDate);
 		transfer.setSchedulingDate(schedulingDate);
 		
-		Transfer response = trasnferRepository.save(transfer);
+		Transfer response = transferRepository.save(transfer);
+		
+		assertNotNull(response);
+	}
+	
+	@Test
+	public void testFindAll() {
+		
+		List<Transfer> response = transferRepository.findAll();
 		
 		assertNotNull(response);
 	}
